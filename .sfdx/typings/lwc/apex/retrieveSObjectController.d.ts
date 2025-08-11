ebbdb9cbfc6a29603217c1361da0e480834c2f62
rvarada-p <varadaravitej@live.in>
2025-08-11 01:48:08 -0500

    ddd

--- .sfdx/typings/lwc/apex/retrieveSObjectController.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@salesforce/apex/retrieveSObjectController.retrieveSObject" {
-  export default function retrieveSObject(param: {recordId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/retrieveSObjectController.checkPermissions" {
-  export default function checkPermissions(): Promise<any>;
-}
-declare module "@salesforce/apex/retrieveSObjectController.updatesObject" {
-  export default function updatesObject(param: {recordId: any, sObjectName: any, fieldsJSON: any}): Promise<any>;
-}
